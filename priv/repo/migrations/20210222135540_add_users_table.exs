a8dd11f81dd873f25fc7e9b6f293865649dc676f
ZZYZX <zzyzx@virtual>
2021-02-22 15:58:04 +0200

    Manual

---
+++ priv/repo/migrations/20210222135540_add_users_table.exs
@@ -0,0 +1,7 @@
+defmodule Hello2.Repo.Migrations.AddUsersTable do
+  use Ecto.Migration
+
+  def change do
+
+  end
+end
---
+++ priv/repo/migrations/20210222135546_add_posts_table.exs
@@ -0,0 +1,7 @@
+defmodule Hello2.Repo.Migrations.AddPostsTable do
+  use Ecto.Migration
+
+  def change do
+
+  end
+end

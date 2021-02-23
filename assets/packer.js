424a3be59c6632c6481082aae140b44998f159c1
ZZYZX <zzyzx@virtual>
2021-02-24 01:31:06 +0200

    Also apply this to JS!

--- assets/packer.js
+++ assets/packer.js
@@ -8,14 +8,17 @@ const fs = require('fs')
 
 const webpackConf = require('./webpack.config.js')
 
-const compiler = webpack(webpackConf(null, { mode: 'development' }))
-
-const watcher = chokidar.watch([__dirname+'/../lib/hello2_web/**/*.css', __dirname+'/../lib/hello2_web/**/*.scss'], {
+const cssWatcher = chokidar.watch([__dirname+'/../lib/hello2_web/**/*.css', __dirname+'/../lib/hello2_web/**/*.scss'], {
     ignored: /(^|[\/\\])\../, // ignore dotfiles
     persistent: true
   });
 
-let paths = []
+const jsWatcher = chokidar.watch(__dirname+'/../lib/hello2_web/**/*.js', {
+    ignored: /(^|[\/\\])\../, // ignore dotfiles
+    persistent: true
+  });
+
+let cssPaths = []
 let cssTimeout = null
 
 const scheduleCssChange = () => {
@@ -28,9 +31,9 @@ const scheduleCssChange = () => {
 }
 
 const handleCssChange = () => {
-    console.log('Building CSS from', paths)
+    console.log('Building CSS from', cssPaths)
     let full_scss = ''
-    paths.forEach(path => {
+    cssPaths.forEach(path => {
         full_scss += fs.readFileSync(path) + '\n'
     })
     const scss_result = sass.renderSync({
@@ -42,18 +45,61 @@ const handleCssChange = () => {
     fs.writeFileSync(output_css_location, minified_result)
 }
 
-const log = console.log.bind(console)
-watcher
-  .on('add', path => { paths.push(path); scheduleCssChange() })
-  .on('change', path => { scheduleCssChange() })
-  .on('unlink', path => { paths = paths.filter(x => x !== path); scheduleCssChange() })
+let jsPaths = []
+let jsTimeout = null
 
-const watching = compiler.watch({
-    // Example [watchOptions](/configuration/watch/#watchoptions)
-    aggregateTimeout: 300,
-    poll: undefined
-}, (err, stats) => { // [Stats Object](#stats-object)
-    // Print watch/build result here...
-    console.log('Build finished')
-    //console.log(stats);
-});
+const scheduleJsChange = () => {
+    if (jsTimeout !== null) {
+        jsTimeout = setTimeout(handleJsChange, 500)
+    } else {
+        jsTimeout = setTimeout(handleJsChange, 0)
+    }
+}
+
+const handleJsChange = () => {
+    restartWebpackWatching(jsPaths)
+}
+
+cssWatcher
+    .on('add', path => { cssPaths.push(path); scheduleCssChange() })
+    .on('change', path => { scheduleCssChange() })
+    .on('unlink', path => { cssPaths = cssPaths.filter(x => x !== path); scheduleCssChange() })
+
+jsWatcher
+    .on('add', path => { jsPaths.push(path); scheduleJsChange() })
+    .on('unlink', path => { jsPaths.push(path); scheduleJsChange() })
+
+let watching = null
+let watchingIndex = 0
+
+const restartWebpackWatching = (additionalPaths) => {
+    const uIndex = ++watchingIndex
+    const doWatch = () => {
+        if (watchingIndex !== uIndex) {
+            return
+        }
+        const compilerConf = webpackConf(null, { mode: 'development' })
+        if (additionalPaths && additionalPaths.length) {
+            compilerConf.entry['app-components'] = additionalPaths
+        }
+        const compiler = webpack(compilerConf)
+        watching = compiler.watch({
+            // Example [watchOptions](/configuration/watch/#watchoptions)
+            aggregateTimeout: 300,
+            poll: undefined
+        }, (err, stats) => { // [Stats Object](#stats-object)
+            // Print watch/build result here...
+            console.log('Build finished')
+            //console.log(stats);
+        });
+    }
+    if (watching !== null) {
+        console.log('Incompatible changes detected, restarting Webpack to adjust...')
+        watching.close(() => doWatch())
+    } else {
+        console.log('Starting Webpack...')
+        doWatch()
+    }
+}
+
+scheduleJsChange()
